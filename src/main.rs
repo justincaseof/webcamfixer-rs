d1a6daa10c99b6591e7bc146d856f335cc80d76b
justin caseof <justin.caseof@gmail.com>
2023-12-09 22:28:35 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 use windows::{
     core::{ComInterface, Interface, GUID, PCWSTR},
     Win32::{
@@ -14,6 +16,10 @@ use windows::{
         },
     },
 };
+
+use crate::consts::camctl::*;
+use crate::consts::guids::*;
+
 
 fn main() {
     windows_1();
@@ -59,6 +65,7 @@ fn windows_1() {
                     
                     // ### PropertyBag ###########################################################################################################################################
                     let mut result__ = ::std::ptr::null_mut();
+                    let mut result__2 = ::std::ptr::null_mut();
                     let hr = m.BindToStorage(None, None, &P_BAG, &mut result__);
                     println!(" --> IPropertyBag as Storage: {:?}", hr);
                     let bag: IPropertyBag = IPropertyBag::from_raw(result__.as_mut().unwrap());
@@ -77,19 +84,35 @@ fn windows_1() {
                     println!("   - pvar: {:?}", prop_val);
                     let val = &(*prop_val).Anonymous.Anonymous;
                     println!("   - bstrVal: {:?}", val.Anonymous.bstrVal);
+                    // no idea if required...
+                    mem::forget(val);
+                    mem::forget(prop_val);
+                    mem::forget(property_name);
+                    mem::forget(pfilter);
 
                     // ###################################################################################################################################################
-                    let mut filter: IBaseFilter;
                     let hr = m.BindToObject(None, None, &BF, &mut result__);
                     println!(" --> IBaseFilter: {:?}  --  result__: {:?}", hr, result__);
                     
                     // ###################################################################################################################################################
-                    let mut iamcc: IAMCameraControl;
-                    let hr = m.BindToObject(None, None, &IAMCC, &mut result__);
-                    println!(
-                        " --> IAMCameraControl: {:?}  --  result__: {:?}",
-                        hr, result__
-                    );
+                    let hr = m.BindToObject(None, None, &IAMCC, &mut result__2);
+                    println!(" --> IAMCameraControl: {:?}  --  result__: {:?}", hr, result__2);
+                    if hr.is_ok() {
+                        let camctl: IAMCameraControl = IAMCameraControl::from_raw(result__2.as_mut().unwrap());
+                        println!("   - camctl: {:?}", camctl);
+                        
+                        let val: *mut i32 = mem::zeroed();
+                        let flags: *mut i32 = mem::zeroed();
+                        let mut the_prop = IAMCameraControl_PROP_ROLL;
+                        for m in 0..10 {
+                            the_prop = m;
+                            let res = camctl.Get(the_prop, val, flags);
+                            println!("   - camprop({}): val={:?}, flags={:?} ({:?})", the_prop, val, flags, res);
+                        }
+
+                        // let res = camctl.Set(IAMCameraControl_FOCUS, IAMCameraControl_FOCUS_OFF, 0);
+                        // println!("   - camctl.set(0): {:?}", res);
+                    }
                 }
                 1 => {
                     println!("end.");
@@ -104,9 +127,28 @@ fn windows_1() {
     }
 }
 
-pub const P_BAG: GUID = IPropertyBag::IID;
-pub const BF: GUID = IBaseFilter::IID;
-pub const IAMCC: GUID = IAMCameraControl::IID;
+#[allow(dead_code, non_upper_case_globals)]
+mod consts {
+    pub mod camctl {
+        pub const IAMCameraControl_PROP_PAN: i32 = 0;
+        pub const IAMCameraControl_PROP_TILT: i32 = 1;
+        pub const IAMCameraControl_PROP_ROLL: i32 = 2;
+        pub const IAMCameraControl_PROP_ZOOM: i32 = 3;
+        pub const IAMCameraControl_PROP_EXPOSURE: i32 = 4;
+        pub const IAMCameraControl_PROP_FOCUS: i32 = 6;
+        pub const IAMCameraControl_FLAG_FOCUS_NONE: i32 = 0x0000;
+        pub const IAMCameraControl_FLAG_FOCUS_AUTO: i32 = 0x0001;
+        pub const IAMCameraControl_FLAG_FOCUS_MANUAL: i32 = 0x0002;
+    }
+    
+    pub mod guids {
+        use windows::{Win32::{System::Com::StructuredStorage::IPropertyBag, Media::DirectShow::{IBaseFilter, IAMCameraControl}}, core::{ComInterface, GUID}};
+        pub const P_BAG: GUID = IPropertyBag::IID;
+        pub const BF: GUID = IBaseFilter::IID;
+        pub const IAMCC: GUID = IAMCameraControl::IID;
+    }
+}
+
 
 // LINKS
 // https://www.appsloveworld.com/cplus/100/66/how-to-get-a-list-of-video-capture-devices-web-cameras-on-windows-c
